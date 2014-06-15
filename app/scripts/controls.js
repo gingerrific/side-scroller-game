1adfe1ccbf45d6a4ce1c4dbf2ec5a549d23fc57d
Josh Date <date0010@me.com>
2014-06-15 01:38:57 -0400

    updating repo to include second block and ability to land on first block. code still needs to be fully commented out and integration with jumping to second block

--- app/scripts/controls.js
+++ app/scripts/controls.js
@@ -1,9 +1,13 @@
+////// the good one
 var sprite = {
-	xCoord:0, yCoord: 0, 
+	xCoord:0, yCoord: 0, // xcoord = left side // ycoord = bottom
+	xCoordRight: 11.875, // xcoordRight = right side
+	yCoordTop: 13.4375,  // ycoordTop = top
 	vX: 0, vY: 0, 
 	direction: 'right',
 	mass: 60, //kg
-	restitution: -0.3
+	restitution: -0.3,
+	platform: 0,
 };
 
 var frameRate = 1/40; // Seconds
@@ -36,21 +40,64 @@ var loop = function () {
 	// Integrate to get velocity
 	sprite.vX += ax*frameRate;
 	sprite.vY += ay*frameRate;
+	
+	if (sprite.vY > -1 && sprite.vY < 1) {
+
+	}
+
+	else {
+		sprite.vY = 0;
+	}
 			
 	// Integrate to get position
-	sprite.xCoord += sprite.vX*frameRate*100;
-	$('.sprite').css({'left': sprite.xCoord+'em'});
-	sprite.yCoord += sprite.vY*frameRate*100;
+	sprite.xCoord += sprite.vX*frameRate*100; // left side
+	sprite.xCoordRight = sprite.xCoord+9.3; // right side
 
-	if (sprite.yCoord <= 0) {
-		$('.sprite').css({'bottom': -sprite.yCoord+'em'});
-		
+	$('.sprite').css({'left': sprite.xCoord+'em'});
+
+	sprite.yCoord += sprite.vY*frameRate*100;
+	sprite.yCoordTop = sprite.yCoord+13.4375;
+	
+	if (sprite.yCoord < 0 ) {
+		$('.sprite').css({'bottom': -sprite.yCoord+(7.8125*sprite.platform)+'em'});
+
+
+		if (sprite.yCoord <= -13 && sprite.xCoord >= 42 && sprite.xcoord <= 56) {
+			console.log(sprite.yCoord,'love me dammit');
+			// sprite.yCoord = 2;
+			// sprite.platform = 2;
+
+			// $('.sprite').css({'bottom': -sprite.yCoord+(7.8125*sprite.platform)+'em'});
+			// $('.sprite').removeClass('sprite-jump-forward');
+			// $('.sprite').removeClass('sprite-jump-backward');
+		}
+
+		else if (sprite.yCoord === -7.332866231710772 && sprite.xCoordRight >= 57) {
+			sprite.yCoord = 1;
+			sprite.platform = 1;
+
+			$('.sprite').css({'bottom': -sprite.yCoord+(7.8125*sprite.platform)+'em'});
+			$('.sprite').removeClass('sprite-jump-forward');
+			$('.sprite').removeClass('sprite-jump-backward');
+			console.log(sprite.yCoord)
+		}
+
 	}
+
 	else {
-		sprite.yCoord = 0
-		$('.sprite').css({'bottom': sprite.yCoord+'em'});
+		sprite.yCoord = 0 + sprite.platform;
+
+		$('.sprite').css({'bottom': (sprite.yCoord - sprite.platform)+(7.8125*sprite.platform)+'em'});
 		$('.sprite').removeClass('sprite-jump-forward');
 		$('.sprite').removeClass('sprite-jump-backward');
+
+		if (sprite.xCoordRight >= 56 && sprite.yCoord == 0) {
+			sprite.vX = 0;
+		}
+		else if (sprite.xCoordRight <= 56.65) {
+			sprite.platform = 0
+		}
+
 	}
 }
 
@@ -72,19 +119,25 @@ $(window).keydown( function (key) {
 	else if (key.keyCode === 39) {
 		event.preventDefault();
 		// Filter out commands if you're at the edge of the board
-		if (sprite.xCoord <= 60) {
+		if (sprite.xCoordRight <= 56 && sprite.yCoord == 0) {
 				sprite.vX = 0.3;
 				sprite.direction = 'right';
+		}
+
+		else if (sprite.yCoord > 0){
+			sprite.vX = 0.3;
+			sprite.direction = 'right';
 		}
 	}
 	// up arrow
 	else if (key.keyCode === 38) {
 		event.preventDefault();
 		// Filter out commands if you're at the edge of the board
-		if (sprite.yCoord == 0) {
+		if (sprite.yCoord == 0 || sprite.yCoord == sprite.platform) {
 				sprite.vY = -1;
 				if (sprite.direction === 'right') {
 					$('.sprite').addClass('sprite-jump-forward');
+
 				}
 				else if (sprite.direction === 'left') {
 					$('.sprite').addClass('sprite-jump-backward');
@@ -98,6 +151,14 @@ $(window).keydown( function (key) {
 		// Filter out commands if you're at the edge of the board
 		if (sprite.yCoord == 0) {
 				sprite.vY = -1;
+
+				if (sprite.direction === 'right') {
+					$('.sprite').addClass('sprite-jump-forward');
+
+				}
+				else if (sprite.direction === 'left') {
+					$('.sprite').addClass('sprite-jump-backward');
+				}
 		}
 	}
 
@@ -114,3 +175,18 @@ $(window).keyup( function (key) {
 		sprite.vX = 0;
 	}
 });
+
+var shape = {}
+
+// coordinates are the left most position of the character/sprite. 
+// also the xCoord is currently in ems, so the right-side position will be xCoord + 11.875em (sprite width)
+// platform width: 11.875em;
+// platform x-axis start: 58em
+// platform height: 7.8125
+// display width: 70em;
+
+
+// true width is 9.3em
+// extra 1.2875 on each side
+// character right toe at box = 47.5 em
+
