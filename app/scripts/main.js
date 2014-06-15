a9cadb8bebf4be98a8f88cbea41f6bb484cd4c85
Jeffrey Dill <jeffdill2@gmail.com>
2014-06-15 18:44:22 -0400

    Added scrolling platform generator

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,35 @@
+'use strict';
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////// INITIALIZE UI
+////////////////////////////////////////////////////////////
 $(document).ready(function() {
-	$('.background').pan({fps: 5, speed: 3, dir: 'left', depth: 50});
-	$('.background').spRelSpeed(5);
+	// $('.background').pan({fps: 5, speed: 3, dir: 'right', depth: 50});
+	// $('.background').spRelSpeed(5);
+
+	loadPlatforms();
 });
+
+////////////////////////////////////////////////////////////
+///////////////////////////////////////////// INITIALIZE APP
+////////////////////////////////////////////////////////////
+var platformCollection = new PlatformCollection();
+var numDelayMilliseconds = 3000;
+var numIntervalID = 0;
+var numLivePlatforms = 0;
+
+function loadPlatforms() {
+	platformCollection.fetch().done(function() {
+		numIntervalID = setInterval(function() {
+			var objPlatformModel = platformCollection.add({});
+			objPlatformModel.save();
+
+			new PlatformView({model: objPlatformModel});
+		}, numDelayMilliseconds);
+	});
+}
+
+function stopPlatformLoad() {
+	clearInterval(numIntervalID);
+	console.log('Platform loading has been stopped.');
+}
---
+++ app/scripts/platform-view.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var PlatformView = Backbone.View.extend({
+	platformTemplate:  _.template($('.platform-template').text()),
+
+	initialize: function() {
+		$('.display-container').append(this.el);
+
+		this.render();
+		this.setLifespan();
+	},
+
+	render: function() {
+		var platformRendered = this.platformTemplate(this.model);
+		this.$el.html(platformRendered);
+		return this;
+	},
+
+	setLifespan: function() {
+		var that = this;
+
+		setTimeout(function() {
+			that.destroy();
+		}, 7000);
+	},
+
+	destroy: function() {
+		this.model.destroy();
+		this.remove();
+	}
+});
